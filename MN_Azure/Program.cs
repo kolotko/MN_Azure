e6259fe0ef8677ad18dff0e0e65ef9cddbfb8c24
kolotko111 <milosznowicki96@wp.pl>
2023-11-22 18:35:10 +0100

    .

--- MN_Azure/Program.cs
+++ MN_Azure/Program.cs
@@ -3,12 +3,12 @@ using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 builder.Services.AddHealthChecks();
 
 var app = builder.Build();
